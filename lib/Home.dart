1af451afc6aae8d664a664f8822dbddae389d6ae
Pedro Falconi <pedrofalck2000@gmail.com>
2022-09-24 18:45:22 -0300

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    _buscaPrecoBTC();
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
